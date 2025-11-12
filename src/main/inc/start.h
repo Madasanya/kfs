c794de9ccd11c08c916d410cce3fe37d9d0fa984
Madasanya <margarete.hanna.mueller@gmail.com>
2025-11-12 15:29:55 +0100

    initial history draft for #20

--- src/main/inc/start.h
+++ src/main/inc/start.h
@@ -3,6 +3,9 @@
 
 #include "inttype.h"
 
+#define SCREEN_WIDTH 80u
+#define SCREEN_HEIGHT 25u
+
 /** @brief Structur for holding informations about kernel. */
 typedef struct __attribute__((packed)) kernel_ver_s
 {
--- src/main/src/kernel.c
+++ src/main/src/kernel.c
@@ -1,6 +1,8 @@
 #include "str_utils.h"
+#include "history.h"
 
 void kernel(void)
 {
+    md_history_init();
     md_put_str("42");
 }
