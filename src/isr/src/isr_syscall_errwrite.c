f0893aa6901fcf10a4508a2f44e7a45448949b03
Madasanya <margarete.hanna.mueller@gmail.com>
2025-12-05 16:21:29 +0100

    Update isr_syscall_errwrite to include level validation in error checks

--- src/isr/src/isr_syscall_errwrite.c
+++ src/isr/src/isr_syscall_errwrite.c
@@ -7,7 +7,7 @@
 
 uint32_t isr_syscall_errwrite(char *arr, uint32_t len, uint32_t lvl)
 {
-    if (arr == NULL || len == 0)
+    if (arr == NULL || len == 0 || lvl >= ERRLOG_LVL_LEN || lvl < 0)
     {
         return 1;
     }
